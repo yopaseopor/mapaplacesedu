fe14bd99c7e3bed7ee691daa888c77a54c86e3fd
yopaseopor <yopaseopor@gmail.com>
2024-11-26 22:40:09 +0100

    test21

--- src/config.js
+++ src/config.js
@@ -1618,8 +1618,8 @@ var config = {
  {
 
    group: 'Primària',
-   title: 'Test19',
-   geojson: 'https://raw.githubusercontent.com/yopaseopor/mapaplacesedu/main/src/primaria20.geojson',
+   title: 'Test21',
+   geojson: 'https://raw.githubusercontent.com/yopaseopor/mapaplacesedu/main/src/primaria21.geojson',
    iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_B1a.png',
    iconStyle: 'background-color:rgba(255,255,255,0.4)',
    style: function (feature) {
@@ -1661,7 +1661,7 @@ var config = {
        })
       })
      },
-     'PERFIL': {
+     'QUALIFICADOR': {
       'ORD': new ol.style.Style({
        zIndex: 100,
        stroke: new ol.style.Stroke({
