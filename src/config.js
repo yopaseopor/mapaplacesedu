254fed492610cfafc43743251bd720ad2bec54ca
yopaseopor <yopaseopor@gmail.com>
2024-11-26 21:10:14 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1618,7 +1618,7 @@ var config = {
  {
 
    group: 'Primària',
-   title: 'Test19',
+   title: 'Test20',
    geojson: 'https://raw.githubusercontent.com/yopaseopor/mapaplacesedu/main/src/primaria18.geojson',
    iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_B1a.png',
    iconStyle: 'background-color:rgba(255,255,255,0.4)',
