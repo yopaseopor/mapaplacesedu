2eaf6c5eaf25f7d7cf583e5422a546404ad7d471
yopaseopor <yopaseopor@gmail.com>
2024-11-26 20:13:13 +0100

    test16

--- src/config.js
+++ src/config.js
@@ -1618,8 +1618,8 @@ var config = {
  {
 
    group: 'Primària',
-   title: 'Test14',
-   geojson: 'https://raw.githubusercontent.com/yopaseopor/mapaplacesedu/main/src/primaria15.geojson',
+   title: 'Test16',
+   geojson: 'https://raw.githubusercontent.com/yopaseopor/mapaplacesedu/main/src/primaria16.geojson',
    iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_B1a.png',
    iconStyle: 'background-color:rgba(255,255,255,0.4)',
    style: function (feature) {
