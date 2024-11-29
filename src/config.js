59198c3fd9407618031614cfe4403a9e9dd8a373
yopaseopor <yopaseopor@gmail.com>
2024-11-30 00:11:20 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1618,7 +1618,7 @@ var config = {
  {
 
    group: 'Primària',
-   title: 'EES4',
+   title: 'EES5',
    geojson: 'https://raw.githubusercontent.com/yopaseopor/mapaplacesedu/main/src/EES4.geojson',
 	iconSrc: imgSrc + 'base/circle.svg',
    iconStyle: 'background-color:rgba(255,255,255,0.4)',
