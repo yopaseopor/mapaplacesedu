7924b7c95deefc75278ec4b49bef39e7c7b1df4c
yopaseopor <yopaseopor@gmail.com>
2024-12-04 20:42:05 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2944,7 +2944,7 @@ var config = {
 				var styles = [];
 
 				/* draw the segment line */ 
-				var width = (parseFloat(maxspeed) / 0.1) + 0.5;
+				var width = (parseFloat(maxspeed) / 0.3) + 0.5;
 				var color = linearColorInterpolation([0, 255, 0], [255, 0, 0], Math.max(maxspeed, 2) / 0.2);
 				var fill = new ol.style.Fill({
 					color: 'rgb(' + color.join() + ')'
